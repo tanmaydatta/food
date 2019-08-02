b2249efa74cafc4f66cf97d0c7bbe98a31cf1bc8
Tanmay Datta <tanmay.datta@grabtaxi.com>
2019-08-02 22:17:10 +0800

    Refactor

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 git pull origin master
+cd cmd/server
 go get .
-go build cmd/server/main.go
-./main
+env GOOS=linux GOARCH=amd64 go build -v .
+#env GOOS=darwin go build -v .
+./server
